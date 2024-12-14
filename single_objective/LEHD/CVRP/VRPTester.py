53a80393158ef49bd167c5e2a01116ad29b158c9
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-12-14 21:49:26 +0600

    0.1

--- single_objective/LEHD/CVRP/VRPTester.py
+++ single_objective/LEHD/CVRP/VRPTester.py
@@ -478,7 +478,7 @@ class VRPTester():
 
         # Simulated Annealing Parameters
         # Iteration: 100
-        T_init = 0.001  # Initial temperature
+        T_init = 0.1  # Initial temperature
         T_min = 1e-5  # Minimum temperature
         alpha = 0.98  # Cooling rate
         temperature = T_init
@@ -611,19 +611,17 @@ class VRPTester():
             # Create a problem name based on solution shape
             name = 'vrp'+str(self.env.solution.shape[1])
 
-            for i in range(5):
-                best_select_node_list, current_best_length = self.construct_initial_solution(batch_size, current_step)
-                print('Get first complete solution!')
-                
-                # Get elapsed time
-                escape_time, _ = clock.get_est_string(1, 1)
+            best_select_node_list, current_best_length = self.construct_initial_solution(batch_size, current_step)
+            print('Get first complete solution!')
+            
+            # Get elapsed time
+            escape_time, _ = clock.get_est_string(1, 1)
 
-                # Log initial solution details
-                self.logger.info("Greedy, name:{}, gap:{:5f} %, Elapsed[{}], stu_l:{:5f} , opt_l:{:5f}".format(name,
-                    ((current_best_length.mean() - self.optimal_length.mean()) / self.optimal_length.mean()).item() * 100, escape_time,
-                    current_best_length.mean().item(), self.optimal_length.mean().item()))
+            # Log initial solution details
+            self.logger.info("Greedy, name:{}, gap:{:5f} %, Elapsed[{}], stu_l:{:5f} , opt_l:{:5f}".format(name,
+                ((current_best_length.mean() - self.optimal_length.mean()) / self.optimal_length.mean()).item() * 100, escape_time,
+                current_best_length.mean().item(), self.optimal_length.mean().item()))
 
-            raise ValueError('Stop here')
             # Iterative solution improvement
             current_best_length = self.iterative_solution_improvement_sa_rrc(
                 episode, clock, name,  batch_size, current_step, best_select_node_list
